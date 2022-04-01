96205315cb236881ccc6f33ee98086a0b9d45f3f
perfecttennails <bgc.info@gmail.com>
2022-04-01 12:45:49 +0000

    add eventlistner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//Wait for the DOM to finish load before running the game
+//Get the button elements and event listners to them
+
+document.addEventListener("DOMcontentLoaded", function)() {
+let button = ducument.getElementeByTagName("button");
+
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+if(this.getAttribute("data-type")==="submit"){
+    alert("You Clicked Submit");
+ } else {
+     let gameType = this.getAttribute ("data-type")
+  }
+    })
+ }
+})
+
+
 function runGame() {
 
 }
