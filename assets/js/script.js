32eba52c99dedbd798498521e6f457c33093e4c6
perfecttennails <bgc.info@gmail.com>
2022-04-01 17:43:34 +0000

    add code to calculate two random numbers for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,29 @@
-//Wait for the DOM to finish load before running the game
-//Get the button elements and event listners to them
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-document.addEventListener("DOMcontentLoaded", function)() {
-let button = ducument.getElementeByTagName("button");
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
-for (let button of buttons) {
-    button.addEventListener("click", function () {
-if(this.getAttribute("data-type")==="submit"){
-    alert("You Clicked Submit");
- } else {
-     let gameType = this.getAttribute ("data-type")
-  }
-    })
- }
-})
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
 
-
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
-
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
@@ -40,14 +46,10 @@ function displayAdditionQuestion() {
 
 }
 
-function displatSubtractQuestion() {
+function displaySubtractQuestion() {
 
 }
 
 function displayMultiplyQuestion() {
-
-}
-
-function displayDivisionQuestion() {
-
+    
 }
