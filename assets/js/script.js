e5569d639543e21af6faecf950d3acaf88c574a0
perfecttennails <bgc.info@gmail.com>
2022-04-07 18:49:43 +0000

    add a higher number to mathfloor

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,8 @@ function runGame(gameType) {
     document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
+    let num1 = Math.floor(Math.random() * 100) + 1;
+    let num2 = Math.floor(Math.random() * 100) + 1;
    
     
 
